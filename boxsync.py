3bbf7e804908816c617dada99adc8aa56f495e15
georgeOy <oyanglulu@gmail.com>
2012-03-11 13:53:48 +0800

    modified even bug fix

--- boxsync.py
+++ boxsync.py
@@ -20,6 +20,7 @@ SHARE = ''
 CONFIG=None
 BOX=None
 ACDATA=None
+LCDATA={}
 BOX_FOLDER_ID=''
 class BoxError(Exception):
     """Exception class for errors received from Facebook."""
@@ -47,8 +48,8 @@ def create_folder(ACDATA,dir):
         create_folder(ACDATA,parent)
 class SyncEventHandler(FileSystemEventHandler):
 
-    moddict={}
-    
+    modiset=set()
+    tomodi=set()
     def on_deleted(self, event):
         """Called when a file or directory is deleted.
         :param event: Event representing file/directory deletion.
@@ -67,6 +68,8 @@ class SyncEventHandler(FileSystemEventHandler):
         box_cwd=BOX_FOLDER+cwd
         if os.path.basename(box_cwd)[0]=='.':
             return
+        if not event.is_directory:
+            self.modiset.add(box_cwd)
         what = 'directory' if event.is_directory else 'file'
         target='folder' if event.is_directory else 'file'
         logging.info("Deleting %s %s", what, cwd)
@@ -92,12 +95,21 @@ class SyncEventHandler(FileSystemEventHandler):
         :param event: Event representing file/directory modification.
         :type event: :class:`DirModifiedEvent` or :class:`FileModifiedEvent` """
         super(SyncEventHandler, self).on_modified(event)
-        cwd = event.src_path.replace(SYNC_FOLDER,'')
+        
+        cwd = event.src_path.replace(SYNC_FOLDER,'').decode('utf-8')
         what = 'directory' if event.is_directory else 'file'
-        
-        
+        box_cwd=BOX_FOLDER+cwd
+        if os.path.basename(box_cwd)[0]=='.':
+            return
         if not event.is_directory:
-            logging.debug("Modifying %s %s", what, cwd)
+            # logging.debug(self.modiset)
+            if box_cwd in self.modiset:
+                logging.debug("[already modi] %s %s", what, self.modiset)
+                self.modiset.clear()
+            else:
+                logging.debug('[to modi] %s',box_cwd)
+                self.on_created(event)
+                self.modiset.add(box_cwd)
             
             #compare file mod time
             
@@ -109,6 +121,10 @@ class SyncEventHandler(FileSystemEventHandler):
         super(SyncEventHandler, self).on_created(event)
         cwd = event.src_path.replace(SYNC_FOLDER,'').decode('utf-8')
         box_cwd=BOX_FOLDER+cwd
+        if os.path.basename(box_cwd)[0]=='.':
+            return
+        if not event.is_directory:
+            self.modiset.add(box_cwd)
         what = 'directory' if event.is_directory else 'file'
         
         try:
@@ -164,7 +180,8 @@ class SyncEventHandler(FileSystemEventHandler):
                 return
             what = 'directory' if event.is_directory else 'file'
             target='folder' if event.is_directory else 'file'
-
+            if not event.is_directory:
+                self.modiset.add(box_cwd)
             if os.path.split(event.src_path)[0]==os.path.split(event.dest_path)[0]:
                 # rename
 
@@ -263,43 +280,59 @@ def full_sync():
             c.close()
             ftw.write(storage.getValue())
 	
-           
+def indexing(LCDATA):
+    for top, dirs, files in os.walk(SYNC_FOLDER):
+        for nm in files:
+            if nm[0]!='.':
+                path = os.path.join(top,nm)
+                box_path  = BOX_FOLDER+path.replace(SYNC_FOLDER,'')
+                LCDATA[box_path]={}
+                LCDATA[box_path]['id']=''
+                LCDATA[box_path]['upadate']=os.path.getmtime(path)
+
 
     
 
 if __name__ == "__main__":
     
-    CONFIG = ConfigParser.RawConfigParser(allow_no_value=True)
+    CONFIG = ConfigParser.RawConfigParser()#allow_no_value=True)
     c=CONFIG.read('config.cfg')
+    default_config="""
+    [UserSetting]
+    sync_path = ~/BoxSync
+    box_path = /TEST
+    log = 20
+    login = false
+    auth_token = nosetyet
+    share = 0
+    """
+    
+
     if len(c)==0:
-        CONFIG.add_section('UserSetting')
-        CONFIG.set('UserSetting','sync_path','~/BoxSync')
-        CONFIG.set('UserSetting','box_path','/TEST')
-        CONFIG.set('UserSetting','log',logging.INFO)
-        CONFIG.set('UserSetting','auth_token')
-        CONFIG.set('UserSetting','share','0')
+        CONFIG.readfp(StringIO(default_config))
         with open('config.cfg', 'wb') as configfile:
             CONFIG.write(configfile)
-        CONFIG.read('config.cfg')
+        # CO# NFIG.read('config.cfg')
     SYNC_FOLDER= CONFIG.get('UserSetting','sync_path')
     SYNC_FOLDER = os.path.expanduser(SYNC_FOLDER)
     BOX_FOLDER = CONFIG.get('UserSetting','box_path')
-    AUTH_TOKEN=CONFIG.get('UserSetting','auth_token')
+  
     SHARE =  CONFIG.get('UserSetting','share')
     BOX= boxdotnet.BoxDotNet()
     logging.basicConfig(level=int(CONFIG.get('UserSetting','log')))
-    logging.debug(AUTH_TOKEN)
-    if AUTH_TOKEN is None:
+    # logging.debug(AUTH_TOKEN)
+    if not CONFIG.getboolean('UserSetting','login'):
         ticket = BOX.login(API_KEY)
-        logging.debug('ticket2='+ticket)
+        # logging.debug('ticket2='+ticket)
         print 'Login to Auth BoxSyn App from the Page Just Popup.'
         while True:
-            done_auth = raw_input('Did you finish?[Y/n]')
+            done_auth = raw_input('Did you Login?[Y/n]')
             if done_auth=='y' or done_auth=='':
                 print 'Login sucessfully! Now you can Sync your files in folder=>'+SYNC_FOLDER
                 rsp = BOX.get_auth_token(api_key=API_KEY, ticket=ticket)
                 if rsp.status[0].elementText=='get_auth_token_ok':
                     AUTH_TOKEN = rsp.auth_token[0].elementText
+                    CONFIG.set('UserSetting','login','true')
                     CONFIG.set('UserSetting','auth_token',AUTH_TOKEN)
                     with open('config.cfg', 'wb') as configfile:
                         CONFIG.write(configfile)
@@ -313,12 +346,16 @@ if __name__ == "__main__":
                 sys.exit(0)
             else:
                 print r'input only "y" or "n" or just press Enter plz.'
+    else:
+        AUTH_TOKEN=CONFIG.get('UserSetting','auth_token')
     try:
+        logging.info('loaded data')
         data_file = open('.data.p','rb')
         ACDATA=pickle.load(data_file)
-        logging.info('load data')
+        logging.info('loaded data')
        
     except:
+        logging.info(actree.status[0].elementText)
         actree = BOX.get_account_tree(api_key=API_KEY,auth_token=AUTH_TOKEN,folder_id=0,params=['simple'])
         logging.info(actree.status[0].elementText)
         print actree.tree[0].elementName
@@ -332,6 +369,17 @@ if __name__ == "__main__":
     actree = BOX.get_account_tree(api_key=API_KEY,auth_token=AUTH_TOKEN,folder_id=BOX_FOLDER_ID,params=['simple'])
     ACDATA = _updata(actree.tree,'0',{},'/')
     logging.debug(ACDATA)
+
+    try:
+        logging.info('indexing sync folers')
+        LCDATA = pickle.load(open('.index.p','rb'))
+        logging.info('read indexing file')
+    except :
+        indexing(LCDATA)
+        with open('.index.p','wb') as data_file:
+            pickle.dump(LCDATA,data_file)
+    logging.debug(LCDATA)
+    logging.info('completed indexing')
     #     actree = BOX.get_account_tree(api_key=API_KEY,auth_token=AUTH_TOKEN,folder_id=0,params=['nozip','simple'])
     #     logging.info(actree.status[0].elementText)
     #     ACDATA = _updata(actree.tree[0].folder[0].folders[0].folder,'0',{},'/')
